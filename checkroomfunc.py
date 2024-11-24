95e34dcedfa051dfaa12eae112e26b49dc2a0b11
Argus8717 <ragall7051@eagle.fgcu.edu>
2024-11-24 08:05:29 -0500

    Create checkroomfunc.py

---
+++ checkroomfunc.py
@@ -0,0 +1,7 @@
+def is_room_recent(self, room, indices_from_right):
+        # If for instance you're checking if a number is in the index -2 or further indices_from_right needs to be 2.
+        # Check the portion of the list before the last `indices_from_right` items.
+        cutoff = len(self.roomsEntered) - indices_from_right
+        if cutoff < 0:
+            cutoff = 0  # Ensure no negative slicing.
+        return room_name in self.roomsEntered[:cutoff]
